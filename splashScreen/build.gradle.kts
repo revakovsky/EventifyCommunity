57df5138acfa5ac841a654372b9e2910a3587cda
@revakovsky <revakovsky@gmail.com>
2023-06-10 17:48:36 +0300

    create SplashScreen module, move SplashScreen file to the appropriate module, add dependencies to the modules

---
+++ splashScreen/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = libs.versions.name.splashscreen.get()
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+
+        testInstrumentationRunner = libs.versions.testInstrumentationRunner.get()
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = libs.versions.jvmTarget.get()
+    }
+}
+
+dependencies {
+
+    implementation(project(path = ":core"))
+
+    implementation(libs.android.coreKtx)
+    implementation(libs.android.appcompat)
+    implementation(libs.android.material)
+
+    implementation(platform(libs.android.composeBom))
+    implementation(libs.bundles.composeUi)
+
+    implementation(libs.compose.navigationCompose)
+
+}
